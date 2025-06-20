c441618b4006602d60d713ec83bfa6b637034830
Vigneshwaran <vvenkateshku@adobe.com>
2025-06-21 00:32:08 +0530

    people-lisitng code added

--- blocks/magazine/magazine.js
+++ blocks/magazine/magazine.js
@@ -10,16 +10,19 @@ export default function decorate(block) {
       items.forEach((item) => {
         const li = document.createElement('li');
         li.innerHTML = `
-          <a href="${item.path}">
+          <a href="${item.path}" class="magazine-item">
             <img src="${item.image}" alt="${item.title}" loading="lazy"/>
-            <h3>${item.title}</h3>
-            <p>${item.description}</p>
+            <h3 class="magazine-title">${item.title}</h3>
+            <p class="magazine-desc">${item.description}</p>
           </a>
         `;
         ul.append(li);
       });
 
       block.innerHTML = '';
+      let title = document.createElement('h2');
+      title.textContent = 'Magazine Articles';
+      block.append(title);
       block.append(ul);
     });
 }
---
+++ blocks/people-listing/people-listing.js
@@ -0,0 +1,17 @@
+export default function decorate(block) {
+    console.log('decorate people listing block', block);
+    Array.from(block.children).forEach((row) => {
+        row.classList.add('people-listing-item-wrapper');
+        Array.from(row.children).forEach((col) => {
+            col.classList.add('people-listing-item');
+            // Add class to <picture>
+            const picture = col.querySelector('picture');
+            if (picture) picture.classList.add('people-listing-picture');
+            // Add class to name field (2nd child)
+            if (col.children[1]) col.children[1].classList.add('people-listing-name');
+            // Add class to social media links (3rd child)
+            if (col.children[2]) col.children[2].classList.add('people-listing-desc');
+            if (col.children[3]) col.children[3].classList.add('people-listing-social');
+        });
+    });
+}
