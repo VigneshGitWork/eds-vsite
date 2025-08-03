23c4ff26d8f87d170210a60ee00be2041e0476f2
Vigneshwaran <vvenkateshku@adobe.com>
2025-08-03 16:16:53 +0530

    lint fix 2

--- blocks/dashboard/dashboard.js
+++ blocks/dashboard/dashboard.js
@@ -2,9 +2,8 @@ export default function decorate(block) {
   const url = '/asserts/d1.json?limit=10';
   fetch(url)
     .then((resp) => resp.json())
-    .then((json) => {
-    }).catch((error) => {
-      console.error('Error fetching magazine data:', error);
+    .then(() => {
+    }).catch(() => {
       const errorMessage = document.createElement('p');
       errorMessage.textContent = 'Failed to load magazine articles. Please try again later.';
       block.append(errorMessage);
